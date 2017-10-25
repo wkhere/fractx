1d11df224cea247629105046e7560171f32dbb96
Wojciech Kaczmarek <wojtekk@kofeina.net>
2017-10-25 21:38:13 +0200

    eliminated one MOVPD by reg. reorder

--- iter_amd64.s
+++ iter_amd64.s
@@ -13,13 +13,13 @@ TEXT ·iterSSE(SB),NOSPLIT,$0-24
     MOVQ    $1, AX
 
     // regs:
-    // X0L - current x, xx
-    // X1L - current y, yy
+    // X0L - x, xx
+    // X1L - y
     // X2L - backup x
-    // X3L - backup y
-    // X4L - backup x0
-    // X5L - backup y0
-    // X6L - backup xx, acc
+    // X3L - yy
+    // X4L - const x0
+    // X5L - const y0
+    // X6L - backup acc
     // X7L - const pbound = 4
 
 loop:
@@ -27,17 +27,16 @@ loop:
     MOVUPD  X1, X3
 
     MULSD   X0, X0      // xx
-    MULSD   X1, X1      // yy
-    MOVUPD  X0, X6
-    ADDSD   X1, X6      // xx+yy
+    MULSD   X3, X3      // yy
+    MOVUPD  X3, X6
+    ADDSD   X0, X6      // xx+yy
     UCOMISD X7, X6      // < 4
     JNB     end
 
-    SUBSD   X1, X0      // xx-yy
+    SUBSD   X3, X0      // xx-yy
     ADDSD   X4, X0      // +x0 = nx
 
-    MOVUPD  X2, X1      // x
-    MULSD   X3, X1      // *y
+    MULSD   X2, X1      // x*y
     ADDSD   X1, X1      // *2
     ADDSD   X5, X1      // +y0 = ny
     INCQ    AX
