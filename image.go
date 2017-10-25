326de3291b9fa369915c43fdcfaafae4862f7c99
Wojciech Kaczmarek <wojtekk@kofeina.net>
2017-10-25 20:42:35 +0200

    go iter: eliminate 1st maxi check
    So now it's exactly as in Asm version.

--- image.go
+++ image.go
@@ -39,12 +39,15 @@ func iterSSE(x0, y0 float64) int64
 
 func iterGo(x0, y0 float64) (i int64) {
 	x, y := x0, y0
-	for i = 1; i < maxi; i++ {
+	for i = 1; ; {
 		xx, yy := x*x, y*y
 		if xx+yy >= 4 {
 			return
 		}
 		x, y = xx-yy+x0, 2*x*y+y0
+		if i++; i >= maxi {
+			break
+		}
 	}
 	return
 }
