6ad7b4b6e338d7ec9c92d6966eee35749dd10ad6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2017-10-27 17:21:28 +0200

    when benchmarking via lib, run only fractal fill, omit image writing

--- image_test.go
+++ image_test.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"image/png"
-	"io/ioutil"
 	"testing"
 )
 
 func BenchmarkImageBW(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		img := NewFractalBW(700, 400, -2.5, -1, 1, 1)
-		png.Encode(ioutil.Discard, img)
+		NewFractalBW(700, 400, -2.5, -1, 1, 1)
 	}
 }
 
 func BenchmarkImageGS(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		img := NewFractalGray(700, 400, -2.5, -1, 1, 1)
-		png.Encode(ioutil.Discard, img)
+		NewFractalGray(700, 400, -2.5, -1, 1, 1)
 	}
 }
