5ac2d0fba0d76fce78f63da6dec09d7e6178b5ca
Wojciech Kaczmarek <wojtekk@kofeina.net>
2019-08-12 18:01:24 +0200

    Fill: calculate float-plane y only once each row

--- fractal.go
+++ fractal.go
@@ -10,8 +10,9 @@ func (f *Fractal) Fill(image FractalImage) {
 	dx := (f.x1 - f.x0) / float64(f.w)
 	dy := (f.y1 - f.y0) / float64(f.h)
 	for y := 0; y < f.h; y++ {
+		cy := f.y0 + float64(y)*dy
 		for x := 0; x < f.w; x++ {
-			i := iter(f.x0+float64(x)*dx, f.y0+float64(y)*dy, f.maxi)
+			i := iter(f.x0+float64(x)*dx, cy, f.maxi)
 			image.writePixel(x, y, i)
 		}
 	}
