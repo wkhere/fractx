ce6964ac3d679a1573ed0d3f690e9bde48b8eb94
Wojciech Kaczmarek <wojtekk@kofeina.net>
2022-01-25 02:45:14 +0100

    test different maxi, in parallel

--- content_test.go
+++ content_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	"github.com/spakin/netpbm"
@@ -14,33 +15,50 @@ func TestPPMContent(t *testing.T) {
 	// create fractx PNG and convert it via imagemagick to PPM:
 	//   convert mandelbrot.png test.ppm
 	// then md5sum PPM file.
-	var tab = []struct {
-		md5      string
-		imageGen func(*Fractal) FractalImage
-	}{
-		{"25bb7c32464ff89ed9773ac479383f66", NewBWImage},
-		{"0bf2783cd5d36fadaf9be441ba3afdbf", NewGrayImage},
+	type tcdata struct {
+		md5   string
+		color string
+		maxi  int
+	}
+	var tab = []tcdata{
+		{"25bb7c32464ff89ed9773ac479383f66", "bw", 200},
+		{"599a7caed7d1bce58a3d37e0127e4278", "bw", 255},
+		{"59567e6b2808da4d2fb8be3b962e9d54", "bw", 256},
+		{"c07a91142c7b1a81db984b0f8ba927b2", "bw", 257},
+		{"fb4559ac9da7a646b1c0c2edf22db01c", "gray", 255},
+		{"0bf2783cd5d36fadaf9be441ba3afdbf", "gray", 200},
+		{"fb4559ac9da7a646b1c0c2edf22db01c", "gray", 256},
+		{"53ae6f53698470da682eb142e0f713cd", "gray", 257},
 	}
 
 	ppmOpt := &netpbm.EncodeOptions{Format: netpbm.PPM}
 
-	for i, tc := range tab {
+	nameGen := func(tc *tcdata) string {
+		return fmt.Sprintf("%s-%d", tc.color, tc.maxi)
+	}
 
-		img := tc.imageGen(fractal)
-		fractal.Fill(img)
+	for _, tc := range tab {
+		tc := tc // pfff
+		t.Run(nameGen(&tc), func(t *testing.T) {
+			t.Parallel()
 
-		hash := md5.New()
-		err := netpbm.Encode(hash, img, ppmOpt)
-		if err != nil {
-			t.Errorf("tc[%d] error during PPM Encode: %v", i, err)
-			continue
-		}
+			fractal := &Fractal{700, 400, -2.5, -1, 1, 1, tc.maxi}
 
-		md5 := hex.EncodeToString(hash.Sum(nil))
+			img := imageGenerators[tc.color](fractal)
+			fractal.Fill(img)
 
-		if md5 != tc.md5 {
-			t.Errorf("tc[%d] MD5 mismatch\nhave %s\nwant %s", i, md5, tc.md5)
-		}
+			hash := md5.New()
+			err := netpbm.Encode(hash, img, ppmOpt)
+			if err != nil {
+				t.Errorf("error during PPM Encode: %v", err)
+				return
+			}
 
+			md5 := hex.EncodeToString(hash.Sum(nil))
+
+			if md5 != tc.md5 {
+				t.Errorf("MD5 mismatch\nhave %s\nwant %s", md5, tc.md5)
+			}
+		})
 	}
 }
