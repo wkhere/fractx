e15e477465ea4c24f7fe028d58dbaa7914fffdb0
Wojciech Kaczmarek <wojtekk@kofeina.net>
2022-05-15 20:14:30 +0200

    content_test: cosmetic reorder tc

--- content_test.go
+++ content_test.go
@@ -25,8 +25,8 @@ func TestPPMContent(t *testing.T) {
 		{"59567e6b2808da4d2fb8be3b962e9d54", "bw", 255},
 		{"c07a91142c7b1a81db984b0f8ba927b2", "bw", 256},
 		{"0e80a022e27d45758defd3626c31c843", "bw", 257},
-		{"53ae6f53698470da682eb142e0f713cd", "gray", 255},
 		{"6a7166e93bb7b91c66bb418087876329", "gray", 200},
+		{"53ae6f53698470da682eb142e0f713cd", "gray", 255},
 		{"53ae6f53698470da682eb142e0f713cd", "gray", 256},
 		{"6f3fc32225cf5a284d2d11fdc73f3deb", "gray", 257},
 	}
